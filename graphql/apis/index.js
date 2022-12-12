2bc1bbcd17a9baedaa77e6d63837d2a223ee6bec
ChenhuaFan <chenhuaf@usc.edu>
2022-12-12 12:46:58 -0800

    add graphql

---
+++ graphql/apis/index.js
@@ -0,0 +1,4 @@
+// const client = new ApolloClient({
+//     uri: "https://api.spacex.land/graphql/",
+//     cache: new InMemoryCache()
+// });
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,34 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import { useQuery, gql } from "@apollo/client";
+import Link from 'next/link';
+import { useMemo } from 'react';
+
+const AuthQuery = (email, password) => gql`
+  {
+    user: authority(email: ${email}, password: ${password}) {
+      _id
+      name
+      email
+      buyerRate
+      sellerRate
+    }
+  }
+`;
+
 export default function Home() {
 
-  fetch('/api/')
-    .then(res => res.json())
-    .then(data => console.log('user', data))
+  const { data, loading, error } = useQuery(AuthQuery('"samcoder1996@gmail.com"', '"d123456f"'));
+
+  const toUserProfile = useMemo(() => {
+    if (loading) return null;
+    else {
+      const { name, _id } = data.user;
+      const formattedName = name.replace(/\b[a-z]/g, (letter) => letter.toUpperCase());
+      return <Link href={`/user/${_id}`}>{formattedName}</Link>
+    }
+  }, [data, loading])
 
   return (
     <div className={styles.container}>
@@ -18,61 +41,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome! {toUserProfile}
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p>F2F is a <b>face-to-face</b> trading platform for second-hand goods</p>
       </main>
-
-
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
